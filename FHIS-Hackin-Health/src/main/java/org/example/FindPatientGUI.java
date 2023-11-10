a5495f753a9a81d9f5f42743cc9b91e26d085527
John Andrew Self <andrew.self31@gmail.com>
2023-11-10 16:40:00 -0500

    added Start of GUI

---
+++ FHIS-Hackin-Health/src/main/java/org/example/FindPatientGUI.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+public class FindPatientGUI extends Application {
+
+    Label firstName;
+    TextField txtFirstName;
+    Label lastName;
+    TextField txtLastName;
+
+    Button btnGetInfo;
+    @Override
+    public void start(Stage primaryStage){
+
+
+    }
+
+}
