5870add05c43eb0065f1137e044a95f1b43fe3c2
John Andrew Self <andrew.self31@gmail.com>
2023-11-11 14:12:48 -0500

    yoyoyoyoy

--- FHIS-Hackin-Health/src/main/java/org/example/CheckInChartManager.java
+++ FHIS-Hackin-Health/src/main/java/org/example/CheckInChartManager.java
@@ -15,4 +15,13 @@ public class CheckInChartManager {
     public List<CheckInChart> getPatientsCheckInCharts() {
         return patientsCheckInCharts;
     }
+    public CheckInChart getCheckInChartByDateRange(String date){
+        for (CheckInChart cc : patientsCheckInCharts){
+            if (cc.getDaterange().equals(date)){
+                return cc;
+            }
+        }
+        return null;
+
+    }
 }
